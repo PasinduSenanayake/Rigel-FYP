d5b5a4bc726c921cac80f98c4ff3cfd62a36549d
piyumi <rameshkafox@gmail.com>
2018-04-17 01:06:58 +0530

    updating ini file to support sys identifier

--- Identifier/ini.py
+++ Identifier/ini.py
@@ -1,5 +1,5 @@
 from loopAnalyzer import loopAnalysis
-
+from systemIdentifier import __systemInformationIdentifier
 response = {
     "returncode":0,
     "error":"",
@@ -8,11 +8,13 @@ response = {
 
 def trigger(filePath,compTimeArguments,runTimeArguments):
     try:
-        responseObj = loopAnalysis(filePath,compTimeArguments,runTimeArguments)
-        if (responseObj['returncode'] == 1):
-            response['error'] = ""
-            response['content'] = responseObj['content']
-            response['returncode'] = 1
+        responseObj = __systemInformationIdentifier()
+        if responseObj['returncode'] == 1:
+            responseObj = loopAnalysis(filePath,compTimeArguments,runTimeArguments)
+            if (responseObj['returncode'] == 1):
+                response['error'] = ""
+                response['content'] = responseObj['content']
+                response['returncode'] = 1
     except Exception as e:
         response['error'] = e
         response['content'] = {}
