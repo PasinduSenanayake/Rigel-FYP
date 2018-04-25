f7626b593536e1347aafb909f97bdc7cacd4c0f9
thulana <thulanakannangara@gmail.com>
2018-04-25 16:58:43 +0530

    few changes

--- Identifier/nestedLoopChecker.py
+++ Identifier/nestedLoopChecker.py
@@ -16,14 +16,19 @@ def nestedloopAnalysis(filePath, compTimeArguments, runTimeArguments):
           #print basicProfile['content'][56]
           #print basicProfile['content'][53]
           threadsUsed= int(str(basicProfile['content'][53]).split(",")[1])
+          parallelPrecentage = float(str(basicProfile['content'][56]).split(",")[1])
           systemIdentifier.__systemInformationIdentifier()
           systhreads=int(systemIdentifier.cpu_info_list["num_cores"])*int(systemIdentifier.cpu_info_list["threads_per_core"])
           if systhreads > threadsUsed:
               print "nested parallelism recommended"
           else:
-              print "nested parallelism not recommended"
+              if parallelPrecentage < 50:
+                  print "nested parallelism recommended "
+              else:
+                  print "nested parallelism not recommended"
     except Exception as e:
       response['error'] = e
       response['content'] = {}
       response['returncode'] = 0
+      print e
     return response
