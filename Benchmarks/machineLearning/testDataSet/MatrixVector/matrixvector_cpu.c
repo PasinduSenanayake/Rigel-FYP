977c02526d6b4642abc776c1a18587aa463e12ab
pasindu <pasinduath@gmail.com>
2018-07-07 14:43:12 +0530

    changed some benchmarks

--- Benchmarks/machineLearning/testDataSet/MatrixVector/matrixvector_cpu.c
+++ Benchmarks/machineLearning/testDataSet/MatrixVector/matrixvector_cpu.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #include <omp.h>
+#include <math.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
   int i,j;
@@ -8,29 +10,29 @@ int main(int argc, char* argv[]) {
   int N = atoi(argv[1]);
   float *b  = (float *)malloc(sizeof(float)*N);
   float *c = (float *)malloc(sizeof(float)*N);
-  float **A = (float **)malloc(N * sizeof(float *));
-  for (i=0; i<N; i++){
-       A[i] = (float *)malloc(N * sizeof(float));
-     }
-
+  float A[N][N];
+  memset(A, 0, N * N * sizeof(float));
 
 
 
 	// computes A*b
   double startTime = omp_get_wtime();
 	#pragma omp parallel for
-  for (i = 0; i <  N; i++){
-      for (j = 0; j < N; j++){
-          A[i][j] = i+j;
+      for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+          A[i][j] = i*N+j*N;
+          b[i] = i*N;
+        }
       }
-      b[i] = i;
-    }
+
 	#pragma omp parallel for
-	for (i=0; i<N; i++) {
-		c[i]=0;
-		for (j=0;j<N;j++) {
-			c[i]=c[i]+A[i][j]*b[j];
-		}
+  for (int k = 0; k < N; k++){
+	   for (int i=0; i<N; i++) {
+		     c[i]=0;
+		    for (int j=0;j<N;j++) {
+			       c[i]=c[i]+A[i][j]*b[j]*k + sin(k)*exp(k)/(k + 1.) + sin(j)*exp(j)/(j + 1.);
+		       }
+      }
 	}
   printf("%f\n",omp_get_wtime()-startTime );
 return 0;
--- Benchmarks/machineLearning/testDataSet/MatrixVector/matrixvector_gpu.c
+++ Benchmarks/machineLearning/testDataSet/MatrixVector/matrixvector_gpu.c
@@ -1,6 +1,9 @@
 
 #include <stdio.h>
 #include <omp.h>
+#include <string.h>
+#include <math.h>
+
 
 int main(int argc, char* argv[]) {
   int i,j;
@@ -8,32 +11,33 @@ int main(int argc, char* argv[]) {
   int N = atoi(argv[1]);
   float *b  = (float *)malloc(sizeof(float)*N);
   float *c = (float *)malloc(sizeof(float)*N);
-  float **A = (float **)malloc(N * sizeof(float *));
-  for (i=0; i<N; i++){
-       A[i] = (float *)malloc(N * sizeof(float));
-     }
-
-
+  float A[N][N];
+  memset(A, 0, N * N * sizeof(float));
 
 
 	// computes A*b
   double startTime = omp_get_wtime();
-  #pragma omp target data map(from:A[0:N][0:N]) map(from:b[0:N]) map(tofrom:c[0:N])
+  #pragma omp target data map(to:A[0:N][0:N]) map(from:b[0:N]) map(tofrom:c[0:N])
+  {
   #pragma omp target teams
-	#pragma omp distribute parallel for
-  for (int i = 0; i <  N; i++){
-      for (int j = 0; j < N; j++){
-          A[i][j] = i+j;
+	#pragma omp distribute parallel for collapse(2)
+      for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+          A[i][j] = i*N+j*N;
+          b[i] = i*N;
+        }
       }
-      b[i] = i;
-    }
+
   #pragma omp target teams
-  #pragma omp distribute parallel for
-	for (int i=0; i<N; i++) {
-		c[i]=0;
-		for (int j=0;j<N;j++) {
-			c[i]=c[i]+A[i][j]*b[j];
-		}
+  #pragma omp distribute parallel for collapse(2)
+  for (int k = 0; k < N; k++){
+	   for (int i=0; i<N; i++) {
+		     c[i]=0;
+		    for (int j=0;j<N;j++) {
+          c[i]=c[i]+A[i][j]*b[j]*k + sin(k)*exp(k)/(k + 1.) + sin(j)*exp(j)/(j + 1.);
+		       }
+      }
+	}
 	}
   printf("%f\n",omp_get_wtime()-startTime );
 
